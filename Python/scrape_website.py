a8befca0e68ce917a94dbdc4f31ef76122c7a833
Nischay mrzn <129774293+Nischaymrzn@users.noreply.github.com>
2024-10-28 12:10:50 +0545

    Create scrape_website.py

---
+++ Python/scrape_website.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+def scrape_website(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return soup.title.string
