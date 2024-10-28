d09a459629b8b46673e1b609fbd1d26cc8a628f2
Nischay mrzn <129774293+Nischaymrzn@users.noreply.github.com>
2024-10-28 12:06:45 +0545

    Create get_weather.py

---
+++ Python/get_weather.py
@@ -0,0 +1,6 @@
+import requests
+def get_weather(city):
+    api_key = "YOUR_API_KEY"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(url)
+    return response.json()
